5281a96e049a444dcfdbeb7885b6ba5ac0fa1a13
mariam.ghazaryan1.y@gmail.com <mariam.ghazaryan1.y@gmail.com>
2026-05-16 11:36:34 +0400

    add engagement endpoint

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import requests
-
+from collections import Counter
 st.set_page_config(
     page_title="User Behavior Analytics Dashboard",
     layout="wide"
